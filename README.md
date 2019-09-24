819d1a6ef849a435eb6c3d74b4452b43c1be26d7
Maxi <maxios123@hotmail.com.ar>
2019-09-24 09:56:33 -0300

    Archivos

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# musicaydatos
-Repositorio del Proyecto "Musica y Ciencia de Datos" 
+# Proyecto Música y Ciencia de Datos
+Proyecto Musica y Ciencia de Datos de Ort.
+## Iniciar sitio web
+Para copiar el source code, hacer `https://github.com/informaticaort/musicaydatos.git`. Después, correr `npm install` para que se agreguen los node_modules. Por último, hacer `npm start`.
--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const fs = require('fs');
 const csv = require('csv-parser');
-const app = express();
 
-app.get('/', (req, res) => {
-	res.setHeader('Content-Type', 'text/html');
-	var inputFilePath = "data1.csv";
-	var file = [];
-	var errors = [];
-	fs.createReadStream(inputFilePath)
-	.pipe(csv())
-	.on('data', function(data){
-			try {
-				file.push(data);
-			}
-			catch(err) {
-				errors.push(err);
-			}
-	})
-	.on('end',function(){
-		res.write("<h1>Datos CSV:</h1>");
-		res.write(JSON.stringify(file));
-		res.write("<h2>Logs de errores:</h2>");
-		res.write(JSON.stringify(errors));
-		res.end();
+const app = express();
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+//app.set('view options', { layout: 'home' });
+app.use(express.static('static'));
+
+console.log("App inciada correctamente, leyendo CSV...");
+var file = [];
+var errors = [];
+fs.createReadStream("data1.csv")
+.pipe(csv())
+.on('data', function(data){
+	try {
+		file.push(data);
+	}
+	catch(err) {
+		errors.push(err);
+	}
+})
+.on('end',function(){
+	console.log("Lectura completada, ejecutando server...");
+	app.listen(80, () => {
+		console.log('Server en puerto 80')
 	});
 });
 
-app.listen(80, () => {
-	console.log('Ejecutando en puerto 80')
+app.get('/', (req, res) => {
+	res.setHeader('Content-Type', 'text/html');
+	res.render("main",{ layout: 'home' });
+});
+
+app.get('/csv', (req, res) => {
+	res.setHeader('Content-Type', 'text/html');
+	res.render("csvout",{layout: 'home',file:JSON.stringify(file),errors:JSON.stringify(errors)});
 });
