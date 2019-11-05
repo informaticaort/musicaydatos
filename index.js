276b441fa99a5e31dcacb9a56273cdf03fe1a0ed
Maxi Os <maxios123@hotmail.com.ar>
2019-11-04 22:29:42 -0300

    Archivos

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const fs = require('fs');
 const csv = require('csv-parser');
+var filename = "final.csv";
 
-const filename = "data1.csv";
 const app = express();
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended:true}));
 console.log("App inciada correctamente, leyendo CSV...");
 var file = [];
 var errors = [];
-var data = [];
+var base = [];
 fs.createReadStream(filename)
 .pipe(csv({ separator: ',' }))
 .on('data', function(data){
@@ -26,8 +26,8 @@ fs.createReadStream(filename)
 })
 .on('end',function(){
 	for(var i in file){
-		file[i].palabras = file[i].palabras.toUpperCase().split(";");
-		data.push({nombre:file[i].nombre,cant:0});
+		file[i].StopWords = file[i].StopWords.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[!\(\)#_$.,\"\'%\/&\s=?¿\[\]¡*¨´{}+-]/g,'').split(";");
+		base.push({nombre:file[i].Canciones,album:file[i].Album,linkhttp:file[i].Link,cant:0});
 	}
 	console.log("Lectura completada, ejecutando server...");
 	app.listen(80, () => {
@@ -41,12 +41,15 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-	var words = req.body.words.toUpperCase().split(",");
-	var out = data.slice();
+	var out = JSON.parse(JSON.stringify(base));
+	var words = [];
+	for(const q in req.body){
+		words.push(...(req.body[q].toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[!#$%&=?¿\[\]¡*¨´{}+-]/g,'').split(/[\s,._\/\(\)\"\'-]+/)));
+	}
 	for(const o in file){
-		for(const p in file[o].palabras){
+		for(const p in file[o].StopWords){
 			for(const i in words){
-				if(file[o].palabras[p]==words[i] || (file[o].palabras[p].includes(words[i]) && words[i].length>3)){
+				if(file[o].StopWords[p]==words[i] || (file[o].StopWords[p].includes(words[i]) && words[i].length>3)){
 					out[o].cant++;
 				}
 			}
@@ -54,10 +57,14 @@ app.post('/', (req, res) => {
 	}
 	out = out.filter((v) => v.cant > 0);
 	out.sort(function ( a, b ){ return b.cant - a.cant; });
+	out = out.slice(0,5);
+	if(out[0])out[0].size = "50";
+	if(out[1])out[1].size = "40";
+	if(out[2])out[2].size = "30";
+	if(out[3])out[3].size = "20";
+	if(out[4])out[4].size = "10";
 	res.setHeader('Content-Type', 'text/html');
 	res.render("out",{ layout: 'home', array: out});
-	//hacer más lindo el template, poner la primera grande, etc
-	//POR ALGUNA RAZÓN DATA SE ESTÁ PASANDO POR REFERENCIA, Y SE VA SUMANDO A ESE
 });
 
 app.get('/csv', (req, res) => {
@@ -68,7 +75,7 @@ app.get('/reload', (req, res) => {
 	console.log("Releyendo csv");
 	file = [];
 	errors = [];
-	fs.createReadStream("data1.csv")
+	fs.createReadStream(filename)
 	.pipe(csv())
 	.on('data', function(data){
 		try {
@@ -78,8 +85,10 @@ app.get('/reload', (req, res) => {
 			errors.push(err);
 		}
 	}).on('end',function(){
+		base = [];
 		for(const i in file){
-			file[i].palabras = file[i].palabras.split(";");
+			file[i].StopWords = file[i].StopWords.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g,"").replace(/[!#$%&=?¿\[\]¡*¨´{}+-]/g,'').split(";");
+			base.push({nombre:file[i].Canciones,album:file[i].Album,linkhttp:file[i].Link,cant:0});
 		}
 		console.log("Lectura completada!");
 		res.redirect("/csv");
